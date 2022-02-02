fb59414c52a506c60602a2f836464b5a521ae78a
Taiwo Oluwaseun <taiwoluwaseun8@gmail.com>
2022-02-02 14:23:14 +0100

    modified users css

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -4,6 +4,8 @@ import Create from '../Create/Create';
 
 import { getUsers, deleteUser } from '../../api';
 
+import trashSvg from "../../images/trash.svg";
+
 const Users = () => {
     const [users, setUsers] = useState([]);
     const [loading, setLoading] = useState(false);
@@ -49,21 +51,38 @@ const Users = () => {
                 <div>
                     {users.map((user) => (
                         <div 
-                            className="mt-8 pb-2 border-b border-purple-100 flex justify-evenly items-center" 
+                            className="mt-8 pb-2 border-b border-purple-100 flex justify-between items-center" 
                             key={user._id}
                         >
-                            <p className="border py-1 px-2 border-purple-800 bg-purple-800 text-white 
-                            font-medium rounded-full mr-5 ml-2">
-                                {user.name_prefix}
-                            </p>
-                            <p className="font-medium mr-20">{user.username}</p>
-                            <p className="font-medium mr-36">{user.first_name} {user.last_name}</p>
-                            <p className="font-medium ml-32">{user.date_of_birth}</p>
-                            <svg onClick={() => handleDeleteUser(user.username)} class="w-5 h-5 ml-5
-                            text-red-500 cursor-pointer" fill="none" stroke="currentColor" viewBox="0 0 24 24" 
-                            xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" 
-                            stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 
-                            7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg>
+                            <div className="ml-5">
+                                <p className="border p-1 border-purple-800 bg-purple-800 text-white 
+                                font-medium rounded-full">
+                                    {user.name_prefix}
+                                </p>
+                            </div>
+                            <div>
+                                <p className="font-medium">
+                                    {user.username}
+                                </p>
+                            </div>
+                            <div className="ml-10">
+                                <p className="font-medium">
+                                    {user.first_name} {user.last_name}
+                                </p>
+                            </div>
+                            <div className="ml-72">
+                                <p className="font-medium">
+                                    {user.date_of_birth}
+                                </p>
+                            </div>
+                            <div className="mr-3">
+                                <img 
+                                    src={trashSvg} 
+                                    alt="trash-icon" 
+                                    class="w-5 h-5 cursor-pointer"
+                                    onClick={() => handleDeleteUser(user.username)}
+                                />
+                            </div>
                         </div>
                     ))}
                 </div>
